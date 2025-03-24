68daae38960682698cfe2f10e9c3570a4e3dda0b
Roopam Wadkar <roopamwadkar219@gmail.com>
2025-03-24 15:17:36 +0530

    library for music links

---
+++ musiclibrary.py
@@ -0,0 +1,8 @@
+music={
+    "shape of you":"https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "despacito":"https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "taki taki":"https://www.youtube.com/watch?v=ixkoVwKQaJg",
+    "lean on":"https://www.youtube.com/watch?v=YqeW9_5kURI",
+    "faded":"https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "closer":"https://www.youtube.com/watch?v=PT2_F-1esPk",
+}
